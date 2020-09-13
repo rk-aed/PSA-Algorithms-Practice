d81a06929cfeb29fa1c2c0f84c9d00f6ed14d1b0
rk-aed <59897405+rk-aed@users.noreply.github.com>
2020-09-12 22:29:17 -0400

    Arrays and strings

---
+++ Practice/src/linked_list/Excercise.java
@@ -0,0 +1,28 @@
+package linked_list;
+
+import java.util.LinkedList;
+
+public class Excercise {
+
+	// Remove dupes
+	public static LinkedList<Integer> removeDupes(LinkedList<Integer> ll){
+		
+		
+		
+		
+		return new LinkedList<Integer>();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+
+	}
+
+}
