abb81fe7c5041afa784058a9e7b5e720f67c3743
Rohit Khokle <khokle.r@northeastern.edu>
2021-03-23 14:14:13 -0400

    Binary Search

--- DS_Practice/src/binarySearch/BinarySearch.java
+++ DS_Practice/src/binarySearch/BinarySearch.java
@@ -8,14 +8,23 @@ public class BinarySearch {
 		int start = 0;
 		int end   = arr.length - 1;
 		
-		
-		
-		
-		
-		
-		
-		
+		while(start >= end) {
+			int mid = start  + ((end - start)/2);
+			
+			if(arr[mid] == element)
+				return mid;
+			else if(arr[mid] > element)
+				end = mid-1;
+			else if(arr[mid] < element)
+				start = mid+1;
+		}
+		return -1;	
 	}
+	
+	
+	
+	
+	
 	
 	public static void main(String[] args) {
 
